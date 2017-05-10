06ebf012df43735ac2b0639bc8ad16f8cb2928fa
Kamil Cieslik <mrfarinq@hotmail.com>
2017-05-10 19:49:37 +0200

    delete namespace std

--- main.cpp
+++ main.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 
-using namespace std;
 
-void displayMenu(const string& info, const string& alghoritm_1, const string& alghoritm_2) //Menu dla tabeli i listy.
+
+void displayMenu(const std::string& info, const std::string& alghoritm_1, const std::string& alghoritm_2) //Menu dla tabeli i listy.
 {
-    cout << endl;
-    cout << info << endl;
-    cout << "1. Wczytaj z pliku." << endl;
-    cout << "2. Wygeneruj graf losowo." << endl;
-    cout << "3. Wyświetl (macierzowo i listowo)." << endl;
-    cout << "4. Algorytm - "<<alghoritm_1<<" (macierzowo i listowo)." << endl;
-    cout << "5. Algorytm - "<<alghoritm_2<<" (macierzowo i listowo)." << endl;
-    cout << "9. Test (pomiary)." << endl;
-    cout << "0. Powrot do menu." << endl;
-    cout << "Podaj opcje:";
+    std::cout << std::endl;
+    std::cout << info << std::endl;
+    std::cout << "1. Wczytaj z pliku." << std::endl;
+    std::cout << "2. Wygeneruj graf losowo." << std::endl;
+    std::cout << "3. Wyświetl (macierzowo i listowo)." << std::endl;
+    std::cout << "4. Algorytm - "<<alghoritm_1<<" (macierzowo i listowo)." << std::endl;
+    std::cout << "5. Algorytm - "<<alghoritm_2<<" (macierzowo i listowo)." << std::endl;
+    std::cout << "9. Test (pomiary)." << std::endl;
+    std::cout << "0. Powrot do menu." << std::endl;
+    std::cout << "Podaj opcje:";
 }
 
 void menu_mst() //Obsługa tabeli.
 {
     
-    string path;
+    std::string path;
     int option;
     do {
         displayMenu("*** Wyznaczanie minimalnego drzewa rozpinającego ***", "Prima", "Kruskala");
-        cin >> option;
-        cout << endl;
+        std::cin >> option;
+        std::cout << std::endl;
         switch (option) {
             case 1: //Tworzenie grafu z pliku txt.
                
@@ -57,12 +57,12 @@ void menu_mst() //Obsługa tabeli.
 void menu_the_shortest_path() //Obsługa tabeli.
 {
     
-    string path;
+    std::string path;
     int option;
     do {
         displayMenu("*** Wyznaczanie najkrótszej ścieżki w grafie ***", "Dijkstry", "Forda-Bellmana");
-        cin >> option;
-        cout << endl;
+        std::cin >> option;
+        std::cout << std::endl;
         switch (option) {
             case 1: //Tworzenie grafu z pliku txt.
                 
@@ -95,14 +95,14 @@ void menu_the_shortest_path() //Obsługa tabeli.
 int main() {
     int option;
     do {
-        cout << endl;
-        cout << "==== MENU GŁÓWNE ===" << endl;
-        cout << "1. Wyznaczanie minimalnego drzewa rozpinającego." << endl;
-        cout << "2. Wyznaczanie najkrótszej ścieżki w grafie." << endl;
-        cout << "0. Wyjscie." << endl;
-        cout << "Podaj opcje:";
-        cin >> option;
-        cout << endl;
+        std::cout << std::endl;
+        std::cout << "==== MENU GŁÓWNE ===" << std::endl;
+        std::cout << "1. Wyznaczanie minimalnego drzewa rozpinającego." << std::endl;
+        std::cout << "2. Wyznaczanie najkrótszej ścieżki w grafie." << std::endl;
+        std::cout << "0. Wyjscie." << std::endl;
+        std::cout << "Podaj opcje:";
+        std::cin >> option;
+        std::cout << std::endl;
         
         switch (option) {
             case 1:
