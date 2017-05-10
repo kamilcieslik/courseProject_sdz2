9a148a94127031340b6ad7026f09ee5e55093aad
Kamil Cieslik <mrfarinq@hotmail.com>
2017-05-10 20:09:04 +0200

    create class which represent Graph; will be implemented by lists of adjacency

---
+++ GraphAdjacencyListsImplementation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mrfarinq on 10.05.17.
+//
+
+#include "GraphAdjacencyListsImplementation.h"
---
+++ GraphAdjacencyListsImplementation.h
@@ -0,0 +1,14 @@
+//
+// Created by mrfarinq on 10.05.17.
+//
+
+#ifndef UNTITLED_GRAPHADJACENCYLISTSIMPLEMENTATION_H
+#define UNTITLED_GRAPHADJACENCYLISTSIMPLEMENTATION_H
+
+
+class GraphAdjacencyListsImplementation {
+
+};
+
+
+#endif //UNTITLED_GRAPHADJACENCYLISTSIMPLEMENTATION_H
